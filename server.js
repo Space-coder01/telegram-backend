00521591c9b88ccd4ef67900535bb4f917bb9730
Space Coder <Space-coder@outlook.com>
2025-06-15 17:51:39 -0400

    Enable CORS so browser can reach API

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
